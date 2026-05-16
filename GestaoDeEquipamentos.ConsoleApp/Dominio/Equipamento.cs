e8a029d157aeed2f0c795bd1f1db8e935064cb94
Mateus Thives <mateus.thives@hotmail.com>
2026-05-16 15:04:27 -0300

    Adicionado Cadastro Equipamento

---
+++ GestaoDeEquipamentos.ConsoleApp/Dominio/Equipamento.cs
@@ -0,0 +1,16 @@
+namespace GestaoDeEquipamentos.ConsoleApp.Dominio;
+
+/*
+    • Deve ter identificador único (id)
+    • Deve ter um nome com no mínimo 6 caracteres;
+    • Deve ter um preço de aquisição;
+    • Deve ter uma fabricante; [X]
+    • Deve ter uma data de fabricação;
+*/
+public class Equipamento
+{
+    public int id;
+    public string nome;
+    public decimal precoAquisicao;
+    public DateTime dataFabricacao;
+}
--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿while (true)
+﻿using GestaoDeEquipamentos.ConsoleApp.Dominio;
+
+while (true)
 {
     Console.Clear();
     Console.WriteLine("---------------------------------");
@@ -21,6 +23,22 @@
 
     if (opcaoMenu == "1")
     {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Cadastro de Equipamentos");
+        Console.WriteLine("---------------------------------");
+        Console.Write("Digite o nome do equipamento:");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite o preço de aquisição do equipamento: ");
+        decimal precoAquisicao = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Digite a data de fabricação do equipamento: ");
+        DateTime dataFabricacao = DateTime.Parse(Console.ReadLine());
+
+        Equipamento equipamento = new Equipamento();
+        equipamento.nome = nome;
+        equipamento.precoAquisicao = precoAquisicao;
+        equipamento.dataFabricacao = dataFabricacao;
     }
     else if (opcaoMenu == "2")
     {
