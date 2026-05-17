adba268c187b7a7da7626790fb701eebf7824684
Mateus Thives <mateus.thives@hotmail.com>
2026-05-17 18:57:10 -0300

    Adicionado classe do domínio de Chamado

---
+++ GestaoDeEquipamentos.ConsoleApp/Dominio/Chamado.cs
@@ -0,0 +1,21 @@
+namespace GestaoDeEquipamentos.ConsoleApp.Dominio;
+
+/* 
+
+  Deve ter um identificador único (id);
+• Deve ter a título do chamado;
+• Deve ter a descrição do chamado;
+• Deve ter um equipamento;
+• Deve ter uma data de abertura;
+
+*/
+
+public class Chamado
+{
+    public int id;
+    public string titulo;
+    public decimal descricao;
+    public DateTime dataAbertura;
+
+    public Equipamento equipamento;
+}
